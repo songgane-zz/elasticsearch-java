a9ffcbe278701e9968cea447af93b434352482a9
SONG JAECHANG <songgane@gmail.com>
2016-03-08 18:36:29 +0900

    Update SearchService.java

--- src/main/java/songgane/elasticsearch/search/SearchService.java
+++ src/main/java/songgane/elasticsearch/search/SearchService.java
@@ -1,11 +1,16 @@
 package songgane.elasticsearch.search;
 
+import org.elasticsearch.action.search.MultiSearchResponse;
+import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilder;
 
 /**
  * Created by songgane on 2016. 1. 4..
  */
 public interface SearchService {
-    SearchResponse search(String index, String type, QueryBuilder query, QueryBuilder filter, int from, int size, boolean explain);
+    SearchResponse matchAll();
+    SearchResponse search(String[] indices, String[] type, QueryBuilder query, QueryBuilder filter, AggregationBuilder[] aggregation, int from, int size, boolean explain);
+    MultiSearchResponse multiSearch(SearchRequestBuilder[] queries);
 }
